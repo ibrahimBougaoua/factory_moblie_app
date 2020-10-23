f0fb601dabbc420fb6de67d28d300f7d7deb8bbc
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-23 03:21:42 -0700

    Refactoring : sharedPreferences

---
+++ lib/controller/sharedPreferences.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class sharedPreferences {
+
+  _storeToken(String token) async
+  {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('token', token);
+  }
+
+  Future<String> getToken() async
+  {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.get('token');
+  }
+
+}
