fafd572e91a793fe4f59e81c1114a755bcb91d47
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-23 05:39:30 -0700

    Refactoring : signin

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,21 @@ import 'package:factory_moblie_app/controller/api.dart';
 void main() async {
   Api i = new Api();
   i.signin('adminstrator@gmail.com','dddd');
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String title = '';
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primaryColor: Colors.amber
       ),
-    ),
-  );
+      home: Signin(title: 'Signin'),
+      routes: <String,WidgetBuilder>{
+        '/signin' : (BuildContext context) => Signin(title)
+      }
+    );
+  }
 }
