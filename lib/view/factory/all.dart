aa9d941963c9076cc31e7d440064141d244c8ad4
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-11-07 05:43:42 -0800

    Refactoring : all factories

--- lib/view/factory/all.dart
+++ lib/view/factory/all.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
-import 'package:folding_cell/folding_cell.dart';
 import 'package:factory_moblie_app/controller/factory.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -27,6 +26,8 @@ class AllState extends State<All> {
     var assetsImage = new AssetImage('assets/images/logo.png'); //<- Creates an object that fetches an image.
     var image = new Image(image: assetsImage, fit: BoxFit.cover);
 
+    final List<String> entries = <String>['A','B','C'];
+    final List<int> colorCodes = <int>[600,500,100];
 
     // TODO: implement build
     return MaterialApp(
@@ -37,10 +38,17 @@ class AllState extends State<All> {
         //),
         body: Container(
           decoration: new BoxDecoration(color: new Color(4280381900)),
-          child: ListView(
-            children: <Widget>[
-
-            ],
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context,int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}'),)
+              );
+            },
+            separatorBuilder: (BuildContext context,int index) => const Divider(),
           ),
         ),
       ),
