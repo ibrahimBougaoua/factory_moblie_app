a144325b77ef0a3ed0a199b673b64756dea0e743
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-11-08 12:18:53 -0800

    Refactoring : all factories

--- lib/view/factory/all.dart
+++ lib/view/factory/all.dart
@@ -24,6 +24,9 @@ class AllState extends State<All> {
       statusBarColor: Colors.transparent,
     ));
 
+    var assetsImage = new AssetImage('assets/images/logo.png'); //<- Creates an object that fetches an image.
+    var image = new Image(image: assetsImage, fit: BoxFit.cover);
+
     Factory f = Factory();
 
     // TODO: implement build
@@ -37,19 +40,24 @@ class AllState extends State<All> {
           future: f.getAllFactorisById(),
           builder: (context ,snapshot){
             if(snapshot.hasError) print(snapshot.error);
-
             var list = snapshot.data;
             return snapshot.hasData
                 ? new ListView.builder(
                 itemCount: list==null?0:list.length,
                 itemBuilder: (context,i){
                   return new Container(
-                      padding: const EdgeInsets.all(10.0),
+                      margin: const EdgeInsets.all(5.0),
+                      padding: const EdgeInsets.all(5),
                       child: new Card(
                         child: new ListTile(
-                          title: new Text(list[i]['name']),
-                          leading: new Icon(Icons.image),
-                          subtitle: new Text('name : ${list[i]['name']}'),
+                          leading: image,
+                          title: new Text(list[i]['name'],style: new TextStyle(fontSize: 15)),
+                          subtitle: new Text(list[i]['created_at'],style: new TextStyle(fontSize: 12)),
+                          trailing:  RaisedButton(
+                            onPressed: () {},
+                        color: new Color(4280381900),
+                        child: new Text('More',style: new TextStyle(color: Colors.white,fontSize: 12)),
+                      ),
                         ),
                       )
                   );
